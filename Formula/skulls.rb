24c7f57bcf5e625d2e612ff94b8e5b51f7b07a27
Kao Félix <me@kaofelix.dev>
2026-02-13 10:07:55 +0100

    skulls v0.1.2

--- Formula/skulls.rb
+++ Formula/skulls.rb
@@ -1,8 +1,8 @@
 class Skulls < Formula
   desc "Dead simple skills installer"
   homepage "https://github.com/kaofelix/skulls"
-  url "https://github.com/kaofelix/skulls/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "ea576d038bd6335f16b6967dcc5c519f2171164d2023ac3f5acdcacf854bff3e"
+  url "https://github.com/kaofelix/skulls/archive/refs/tags/v0.1.2.tar.gz"
+  sha256 "15faa2f919223b8d76f5a3a9771c37d725fa36e781d67f27ed1566d74aa5d514"
 
   depends_on "go" => :build
 
